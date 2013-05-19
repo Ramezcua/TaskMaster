a6f6f456a71c3ed57252bfd0b5c7f64c0ec58439
Ricco Amezcua <ramezcua7@gmail.com>
2013-05-19 16:02:38 -0500

    Finished TaskList class

--- src/en/ramezcua/taskmaster/core/TaskList.java
+++ src/en/ramezcua/taskmaster/core/TaskList.java
@@ -4,26 +4,49 @@ import java.util.ArrayList;
 
 public class TaskList {
 	
-	private ArrayList tasks = new ArrayList();
+	private ArrayList<Task> tasks = new ArrayList<Task>();
 	
 	TaskList(){
 
 	}
 	
-	public void addTask(){
-		
+	public int numberOfTasks(){
+		return tasks.size();
 	}
 	
-	public void removeTask(){
-		
+	public Task getTask(int index){
+		return tasks.get(index);
+	}
+	
+	public void addTask(){
+		Task task = new Task();
+		this.tasks.add(task);
+	}
+	
+	public void addTask(String description){
+		Task task = new Task(description);
+		this.tasks.add(task);
+	}
+	
+	public void removeTask(int index){
+		this.tasks.remove(index);
+	}
+	
+	public void removeTask(Task task){
+		this.tasks.remove(task);
 	}
 	
 	public void removeCompletedTasks(){
 		
+		for(int i = 0; i < this.tasks.size(); i++){
+		
+			Task task = getTask(i);
+			if (task.getCompletion() == true){
+				removeTask(i);
+			}
+		}
+		
+		
 	}
 	
-	
-	
-	
-
 }
