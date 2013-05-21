7714046559ad0b45b08da8ab5f57da326627e3ce
Ricco Amezcua <ramezcua7@gmail.com>
2013-05-20 19:40:58 -0500

    Have a window up from JavaSwing.  Still need to add check boxes

--- src/TaskMaster.java
+++ src/TaskMaster.java
@@ -1,7 +1,35 @@
 
+import javax.swing.*;
+import java.awt.*;
+
+
 public class TaskMaster {
 	public static void main(String[] args) {
-
+		
+		JFrame window = new JFrame("Task Master");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(500, 700);
+		window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.PAGE_AXIS));
+		
+		JPanel topButtons = new JPanel();
+		JPanel taskPanel = new JPanel();
+		
+		topButtons.setLayout(new FlowLayout());
+		
+		JButton newTaskButton = new JButton("New Task");
+		JButton removeCompletedTasksButton = new JButton("Remove Completed Tasks");
+		
+		topButtons.add(newTaskButton);
+		topButtons.add(removeCompletedTasksButton);
+		
+		JTextArea text = new JTextArea("Get the milk");
+		
+		taskPanel.add(text);
+		
+		window.add(topButtons);
+		window.add(Box.createRigidArea(new Dimension(0,5)));
+		window.add(taskPanel);
+		window.setVisible(true);
 	}
 
 }
